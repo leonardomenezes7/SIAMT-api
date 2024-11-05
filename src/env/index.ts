3f53b4dcb7eec1370e4ee3cc08824d2cc90bf7e9
Leonardo <leonardomenezes47@outlook.com>
2024-11-04 22:56:08 -0400

    prepare deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,8 @@ import { z } from "zod"
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
   DATABASE_URL: z.string().refine(value => value.trim() !== ""),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"])
 })
 
 const _env = envSchema.safeParse(process.env)
