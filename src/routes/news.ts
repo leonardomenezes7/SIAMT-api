0a6ce08a3f148ed0e1cb9f4966a7ce57564379c9
Leonardo <leonardomenezes47@outlook.com>
2024-11-04 23:39:57 -0400

    fix: tmp folder not found on productionnnnnnnnmn

--- src/routes/news.ts
+++ src/routes/news.ts
@@ -43,9 +43,7 @@ export async function newsRoutes(app: FastifyInstance) {
         return reply.status(400).send({ message: 'Image is required' })
       }
 
-      const tmpDir = path.join(__dirname, '../tmp')
-
-      const filePath = path.join(tmpDir, imageFileName)
+      const filePath = path.join(__dirname, "../tmp", imageFileName)
 
       await writeFile(filePath, imageFileBuffer);
 
